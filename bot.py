77cb2a4b4a71152279d1de169fc00923fcf3c5de
Thegamesbg <33571437+Thegamesbg@users.noreply.github.com>
2018-10-06 01:04:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -244,7 +244,7 @@ async def reminder(ctx):
 async def kiss(ctx, username, *reason):
     if len(ctx.message.mentions) > 0:
       user = ctx.message.mentions[0]
-      msgs = ["https://cdn.discordapp.com/attachments/496005430254764042/497877908749287450/Untitled-1.png", "https://cdn.discordapp.com/attachments/435485699140091906/497884677168693278/discord-animated-avatar.gif"]
+      msgs = ["https://cdn.discordapp.com/attachments/496005430254764042/497891785323053056/discord-avatar30092018.png", "https://cdn.discordapp.com/attachments/496005430254764042/497891785323053056/discord-avatar30092018.png"]
       rsn = " ".join(reason)
       rdm = random.choice(msgs)
       embed = discord.Embed(title="**{}** has been kissed by **{}** for **{}**.".format(user.name, ctx.message.author.name, rsn), color=0xfe2ef7)
