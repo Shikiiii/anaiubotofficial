a0f0f1ed06f33aa5231797a9f5dcd7996d12ea5d
Thegamesbg <33571437+Thegamesbg@users.noreply.github.com>
2018-10-16 22:39:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -425,8 +425,8 @@ async def report(ctx, user, reason, *message):
         reporting.set_author(name="A new report has appeared:", icon_url=bot.user.avatar_url)
         reporting.add_field(name="Rule broken: {}".format(reason), value="Additional information (optimal): {}".format(" ".join(message)))
         await bot.send_message(reporting_channel, "", embed=reporting)
-        reply = discord.Embed(title="Thank you! Your report has been send to our staff team for a review.", description="", color=3adf00)
-        reply.set_footer(text="Please note that reports that are NOT TRUE will transfer the punishment to YOU!")
+        reply = discord.Embed(color=3adf00)
+        reply.add_field(name="Thank you! Your report has been send to our staff team for a review.", value="Please note that reports that are NOT TRUE will transfer the punishment to YOU!")
         await bot.send_message(ctx.message.channel, "", embed=reply)
     else
         await bot.say(":x: | Nu, baka! Correct usage: ``!report @someone rule_broken optimal_information``. Example: ``!report @Thegamesbg#2392 3 He is spamming and doesn't want to stop!``
