40c82fb6aa218be2755ffe11bce0c84c40e34d50
Thegamesbg <33571437+Thegamesbg@users.noreply.github.com>
2018-10-17 13:28:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -422,7 +422,7 @@ async def report(ctx, user, reason, *message):
     if len(ctx.message.mentions) > 0:
         user = ctx.message.mentions[0]
         await bot.send_message(ctx.message.channel, "Type ``confirm`` if you are sure you want to report this member. Please note that reports that are NOT TRUE will transfer the punishment to YOU, so please don't abuse this command.")
-        msg = await client.wait_for_message(author=ctx.message.author, content='confirm')
+        msg = await bot.wait_for_message(author=ctx.message.author, content='confirm')
         reporting_channel = discord.utils.get(ctx.message.author.server.channels, name="report-logs")
         reporting = discord.Embed(color=0xff0000)
         reporting.set_author(name="A new report has appeared:", icon_url=bot.user.avatar_url)
